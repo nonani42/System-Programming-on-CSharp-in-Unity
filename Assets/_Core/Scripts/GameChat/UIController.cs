537fb40fca358499d1ba5ebd6f640995afc87ad5
nonani <42.nonani@gmail.com>
2024-01-11 17:18:20 +0300

    Lesson 3

---
+++ Assets/_Core/Scripts/GameChat/UIController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace GameChat
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] private Button buttonStartServer;
+        [SerializeField] private Button buttonShutDownServer;
+        [SerializeField] private Button buttonConnectClient;
+        [SerializeField] private Button buttonDisconnectClient;
+        [SerializeField] private Button buttonSendMessage;
+        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private TextField textField;
+        [SerializeField] private Server server;
+        [SerializeField] private Client client;
+
+        private void Start()
+        {
+            buttonStartServer.onClick.AddListener(() => StartServer());
+            buttonShutDownServer.onClick.AddListener(() => ShutDownServer());
+            buttonConnectClient.onClick.AddListener(() => Connect());
+            buttonDisconnectClient.onClick.AddListener(() => Disconnect());
+            buttonSendMessage.onClick.AddListener(() => SendMessage());
+            client.onMessageReceive += ReceiveMessage;
+        }
+
+        private void StartServer()
+        {
+            server.StartServer();
+        }
+
+        private void ShutDownServer()
+        {
+            server.ShutDownServer();
+        }
+
+        private void Connect()
+        {
+            client.Connect();
+        }
+
+        private void Disconnect()
+        {
+            client.Disconnect();
+        }
+
+        private void SendMessage()
+        {
+            client.SendMessage(inputField.text);
+            inputField.text = "";
+        }
+
+        public void ReceiveMessage(object message)
+        {
+            textField.ReceiveMessage(message);
+        }
+    }
+}
