e355e41cf8fc4d622fe05f422caf1de1df05f4d3
nonani <42.nonani@gmail.com>
2024-01-14 21:38:45 +0300

    Lesson 4

---
+++ Assets/_Core/Scripts/Multiplayer/Character.cs
@@ -0,0 +1,39 @@
+﻿using Mirror;
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Multiplayer
+{
+    [RequireComponent(typeof(CharacterController))]
+    public abstract class Character : NetworkBehaviour
+    {
+        protected Action OnUpdateAction { get; set; }
+        protected abstract FireAction fireAction { get; set; }
+
+        [SyncVar] protected Vector3 serverPosition;
+
+        protected virtual void Initiate()
+        {
+            //OnUpdateAction += Movement;
+        }
+
+        private void Update()
+        {
+            //OnUpdate();
+        }
+
+        private void OnUpdate()
+        {
+            //OnUpdateAction?.Invoke();
+        }
+
+        [Command]
+        protected void CmdUpdatePosition(Vector3 position)
+        {
+            //serverPosition = position;
+        }
+
+        public abstract void Movement();
+    }
+}
---
+++ Assets/_Core/Scripts/Multiplayer/Player.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Multiplayer
+{
+    public class Player : NetworkBehaviour
+    {
+        [SerializeField] private GameObject playerPrefab;
+
+        private GameObject playerCharacter;
+
+        private void Start()
+        {
+            SpawnCharacter();
+        }
+
+        public void SpawnCharacter()
+        {
+            if (!isServer)
+                return;
+
+            //playerCharacter = Instantiate(playerPrefab);
+            //NetworkServer.SpawnWithClientAuthority(playerCharacter, connectionToClient);
+        }
+    }
+}
